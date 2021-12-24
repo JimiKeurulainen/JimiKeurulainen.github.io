08da5f1c6add58eec8b99d9c48c06eab0d094202
Jimi Keurulainen <jimi.keurulainen@hotmail.fi>
2021-12-24 23:22:14 +0200

    Navbar now detects different buttons

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -2,6 +2,10 @@ import React, { useState } from 'react';
 import '../css/index.css';
 
 function Carousel() {
+  const picArr = [];
+  for (var i = 0; i < 10; i++) {
+    
+  }
   const [count, setCount] = useState(0);
 
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,38 @@
 import React, { useState } from 'react';
 import '../css/index.css';
 
+function Btn(props) {
+  const btnStyle = {
+    marginTop: `${props.btnStyle}vh`,
+  };
+  const [color, setColor] = useState(false);
+  const toggleColor = () => setColor(value => !value);
+
+  color === true ? btnStyle.color = "blue" : btnStyle.color = "black";
+
+  if (color === true && props.active === true) {
+    return (
+      <button style={btnStyle} onClick={() => {props.func(); toggleColor();}}>
+        {props.name}
+      </button>
+    )
+  }
+  else if (props.active === false) {
+    return (
+      <button style={btnStyle} onClick={() => {props.func(); toggleColor();}}>
+        {props.name}
+      </button>
+    )
+  }
+  else if (props.active === true) {
+    return (
+      <button style={btnStyle} onClick={() => {toggleColor();}}>
+        {props.name}
+      </button>
+    )
+  }
+}
+
 function Navbar() {
   const { innerHeight: height } = window;
   let margin = 0;
@@ -8,7 +40,7 @@ function Navbar() {
   const togglePos = () => setPos(value => !value);
 
   if (pos === true) {
-    margin = 20;
+    margin = 15;
     window.scrollTo(0, height);
   }
   else {
@@ -16,21 +48,11 @@ function Navbar() {
     window.scrollTo(0, 0);
   }
 
-  const btnStyle = {
-    marginTop: `${margin}vh`
-  }
-
   return (
     <div id='Navbar'>
-      <button style={btnStyle} onClick={togglePos}>
-        GALLERY
-      </button>
-      <button style={btnStyle} onClick={togglePos}>
-        ABOUT ME
-      </button>
-      <button style={btnStyle} onClick={togglePos}>
-        CONTACT
-      </button>
+      <Btn name="GALLERY" func={togglePos} active={pos} btnStyle={margin} />
+      <Btn name="ABOUT ME" func={togglePos} active={pos} btnStyle={margin} />
+      <Btn name="CONTACT" func={togglePos} active={pos} btnStyle={margin} />
     </div>
   );
 }
